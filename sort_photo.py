6a0673147837c476692d74c15d060f874085854f
Nikolai <61523510+Moddeuss@users.noreply.github.com>
2020-05-07 22:41:42 +0200

    Update sort_photo.py

--- sort_photo.py
+++ sort_photo.py
@@ -3,7 +3,7 @@ import os, time, shutil
 class Parcing_Foto:
 
     def path(self):
-        self.path = input('Введите путь откуда сортировать фотографии: ')
+        self.path = input('Enter the path to sort files: ')
         self.path_normal = os.path.normpath(self.path)
         self.count = 0
 
@@ -19,7 +19,7 @@ class Parcing_Foto:
                 os.makedirs(self.path_normal + '\\' + str(file_time[0]) + '\\' + str(file_time[1]), exist_ok=True)
                 path2 = self.path_normal + '\\' + str(file_time[0]) + '\\' + str(file_time[1])
                 shutil.copy2(self.path + '\\' + file, path2 + '\\' + file)
-        print('Скопированно файлов: ', self.count // 2)
+        print('Copied files: ', self.count // 2)
 
 sum = Parcing_Foto()
 sum.path()
